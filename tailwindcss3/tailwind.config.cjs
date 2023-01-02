ff8a251aa2809e1bed80b39464c9ae77631fd067
YoannaPo <yoannna.pol@gmail.com>
2023-01-02 18:01:58 +0000

    Extend colour palette

--- tailwindcss3/tailwind.config.cjs
+++ tailwindcss3/tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/index.html', './src/**/*.{vue, js, ts, jsx, tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'indigo': colors.indigo,
+        cyan: colors.cyan,
+
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "linear-gradient(to right bottom, rgba(75, 0, 130, 0.9), rgba(43, 108, 176, 0.8)), url('https://i.ibb.co/8xsq1vV/brooke-cagle-u-HVRv-Dr7pg-unsplash.jpg')",
+        
+        'section-pattern':
+           "linear-gradient(to right bottom, rgba( 33, 5, 32, 0.9), rgba(0, 0, 14, 0.8)), url('https://i.ibb.co/4Twf3w9/brooke-cagle-people-laptops.jpg')",
+      },
+
+    },
   },
   plugins: [],
 }
